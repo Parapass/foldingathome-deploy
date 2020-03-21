d508c38ebbc9f7105a88c412b2b3f8f4c7ed74a7
Parapass <parapasser@gmail.com>
2020-03-20 20:48:49 -0400

    Create deploy-noviewer.sh

---
+++ bash/deploy-noviewer.sh
@@ -0,0 +1,5 @@
+wget https://download.foldingathome.org/releases/public/release/fahclient/debian-stable-64bit/v7.5/fahclient_7.5.1_amd64.deb
+wget https://download.foldingathome.org/releases/public/release/fahcontrol/debian-stable-64bit/v7.5/fahcontrol_7.5.1-1_all.deb
+
+sudo dpkg -i --force-depends fahclient_7.5.1_amd64.deb
+sudo dpkg -i --force-depends fahcontrol_7.4.4_amd64.deb
